a5a8011aac13c4a7baa2b99755270da53e5f801f
Breno Sarkis <sarkisbreno@gmail.com>
2018-07-29 12:40:40 +0200

    First leap year

---
+++ Kata-LeapYears/Kata.LeapYears/LeapYearsValidatorShould.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+namespace Kata.LeapYears
+{
+    [TestFixture]
+    public class LeapYearsValidatorShould
+    {
+        [Test]
+        public void ValidateLeapYears()
+        {
+            Assert.That(LeapYearsValidator.IsLeapYear(2000), Is.EqualTo(true));
+        }
+    }
+
+    public class LeapYearsValidator
+    {
+        public static bool IsLeapYear(int year)
+        {
+            return true;
+        }
+    }
+}
